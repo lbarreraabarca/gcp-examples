b51c1eae42174071fcfc130dc4ec2712e80dd08d
lbarreraabarca <lu.barreraabarca@gmail.com>
2020-04-05 17:44:47 -0300

    [ feat/word-count-java]: Fix args execution.

--- dataflow/word-count/mvn-compile.sh
+++ dataflow/word-count/mvn-compile.sh
@@ -4,8 +4,8 @@ export PROJECT_ID=my-test-project-270713
 export BUCKET_NAME=lbarrera-1
 
 mvn compile exec:java \
-    -Dexec.mainClass=com.example.wordCount \
-    -Dexec.args="--project=${PROJECT_ID}" \
+    -Dexec.mainClass=com.example.WordCount \
+    -Dexec.args="--project=${PROJECT_ID} \
     --stagingLocation=gs://${BUCKET_NAME}/staging/ \
     --output=gs://${BUCKET_NAME}/output \
-    --runner="BlockingDataflowPipelineRunner"
+    --runner=BlockingDataflowPipelineRunner"
