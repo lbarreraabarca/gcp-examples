f4e3b89dda41a432724182f0bdad1cd9ea6dc129
lbarreraabarca <lu.barreraabarca@gmail.com>
2020-04-07 15:08:23 -0300

    [feat/dataproc-pyspark]: Hard codding textFile read.

--- dataproc/readFileSpark/src/main/scala/com/myCompany/scala/MaxPrice.scala
+++ dataproc/readFileSpark/src/main/scala/com/myCompany/scala/MaxPrice.scala
@@ -8,7 +8,7 @@ object MaxPrice {
     val conf = new SparkConf().setAppName("Max Price")
     val sc = new SparkContext(conf)
 
-    sc.textFile(args(0))
+    sc.textFile("gs://lbarrera-1/scala-jars/readFileSpark/input/PAMP.BA.csv")
       .map(_.split(","))
       .map(rec => ((rec(0).split("-"))(0).toInt, rec(1).toFloat))
       .reduceByKey((a,b) => Math.max(a,b))
