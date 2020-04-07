07952a0bb518b629fc71d9fe8038e65cdd9d05dc
lbarreraabarca <lu.barreraabarca@gmail.com>
2020-04-07 13:42:54 -0300

    [feat/dataproc-pyspark]: Fix directory write to bucket.

--- dataproc/readFileSpark/compile_gcloud.sh
+++ dataproc/readFileSpark/compile_gcloud.sh
@@ -6,7 +6,7 @@ export PROJECT_NAME=readFileSpark
 mvn clean package
 
 # Copy in Bucket jars files.
-gsutil cp target/*.jar gs://${BUCKET_NAME}/scala-jars/${PROJECT_NAME}
+gsutil cp target/*.jar gs://${BUCKET_NAME}/scala-jars/${PROJECT_NAME}/
 
 # Copy in Bucket inout files
-gsutil cp input/PAMP.BA.csv gs://${BUCKET_NAME}/scala-jars/${PROJECT_NAME}/
+gsutil cp input/PAMP.BA.csv gs://${BUCKET_NAME}/scala-jars/${PROJECT_NAME}/input/
