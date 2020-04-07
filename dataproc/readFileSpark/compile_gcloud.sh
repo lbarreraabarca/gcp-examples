fcd3b4c0d4c43b146a37ef665bef68ea49e5a6f0
lbarreraabarca <lu.barreraabarca@gmail.com>
2020-04-07 13:07:41 -0300

    [feat/dataproc-pyspark]: readFileSpark pipeline compile with mvn package.

---
+++ dataproc/readFileSpark/compile_gcloud.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+export BUCKET_NAME=lbarrera-1
+export PROJECT_NAME=readFileSpark
+
+mvn clean package
+
+# Copy in Bucket jars files.
+gsutil cp target/*.jar gs://${BUCKET_NAME}/scala-jars/${PROJECT_NAME}
+
+# Copy in Bucket inout files
+gsutil cp input/PAMP.BA.csv gs://${BUCKET_NAME}/scala-jars/${PROJECT_NAME}/
