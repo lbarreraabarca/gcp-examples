28d3d46352e55e6aa4300ed7fa3e6b707f3ff6ad
lbarreraabarca <lu.barreraabarca@gmail.com>
2020-04-07 15:17:19 -0300

    [feat/dataproc-pyspark]: Add println logger.

--- dataproc/readFileSpark/src/main/scala/com/myCompany/scala/MaxPrice.scala
+++ dataproc/readFileSpark/src/main/scala/com/myCompany/scala/MaxPrice.scala
@@ -8,6 +8,7 @@ object MaxPrice {
     val conf = new SparkConf().setAppName("Max Price")
     val sc = new SparkContext(conf)
 
+    println("Init spark process")
     sc.textFile("gs://lbarrera-1/scala-jars/readFileSpark/input/PAMP.BA.csv")
       .map(_.split(","))
       .map(rec => ((rec(0).split("-"))(0).toInt, rec(1).toFloat))
