04f8cfe14b53bf68724649ca9fbf0f30bdad99e8
lbarreraabarca <lu.barreraabarca@gmail.com>
2020-04-07 15:26:27 -0300

    [feat/dataproc-pyspark]: Add gsutil command for delete output data from spark pipeline.

--- dataproc/readFileSpark/compile_gcloud.sh
+++ dataproc/readFileSpark/compile_gcloud.sh
@@ -10,3 +10,5 @@ gsutil cp target/*.jar gs://${BUCKET_NAME}/scala-jars/${PROJECT_NAME}/
 
 # Copy in Bucket inout files
 gsutil cp input/PAMP.BA.csv gs://${BUCKET_NAME}/scala-jars/${PROJECT_NAME}/input/
+
+gsutil rm gs://${BUCKET_NAME}/scala-jars/${PROJECT_NAME}/output/
